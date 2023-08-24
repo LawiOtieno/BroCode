1d6e621579263b10bd20bd48c83f0831c80e5162
LAWRENCE OTIENO <laurielawi12@gmail.com>
2023-08-24 19:59:11 +0300

    Math Functions

--- Python-Full-course/6-math_functions.py
+++ Python-Full-course/6-math_functions.py
@@ -2,3 +2,33 @@
 
 # Second line 👆🏼 should be blank
 #An empty line should be added at the end
+'''
+Math Functions
+    Inbuilt mathematical operations 
+'''
+import math # Import math module
+
+print(math.pi)
+print() # Blank line, also print("")
+
+pi = 3.141592653589793 # Output of above print
+print(round(pi)) # Round to the nearest whole number
+print(math.ceil(pi)) # Upper limit
+print(math.floor(pi)) # Lower limit
+print(math.sqrt(pi)) # Find square root
+print(pow(pi, 2)) # Find square
+print() # Blank line, also print("")
+
+# Multiple integers
+a = 2
+b = 34
+c = 100
+print(max(a,b,c))
+print(min(a,b,c))
+print(math.gcd(a,b,c))
+print(math.lcm(a,b,c))
+print() # Blank line, also print("")
+
+# Absolute value
+temp = -15
+print(abs(temp))
