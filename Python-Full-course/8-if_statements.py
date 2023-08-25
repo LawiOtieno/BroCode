f3e5a16ea81356de0519a263ab6d750b2a9f4e2b
LAWRENCE OTIENO <laurielawi12@gmail.com>
2023-08-25 19:12:26 +0300

    if else Statement

--- Python-Full-course/8-if_statements.py
+++ Python-Full-course/8-if_statements.py
@@ -2,3 +2,19 @@
 
 # Second line 👆🏼 should be blank
 #An empty line should be added at the end
+'''
+if - Statement
+    A block of code that will be be executed if a particular condition is met
+'''
+age = int(input("What is your age? "))
+
+if age == 100:
+    print("You are a century old.")
+elif age >= 18:
+    print("You are an adult.")
+elif age == 0:
+    print("You are a new born child. Happy birthday!!!")
+elif age < 0:
+    print("Please enter valid age! Unborn don't live.")
+else:
+    print("You are a child.")
