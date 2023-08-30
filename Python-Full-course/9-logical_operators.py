f60a1c4f551a5a2ca6f15b9c88433216751add99
LAWRENCE OTIENO <laurielawi12@gmail.com>
2023-08-30 18:05:35 +0300

    Logical Operators

--- Python-Full-course/9-logical_operators.py
+++ Python-Full-course/9-logical_operators.py
@@ -1,4 +1,15 @@
 # The first line in Python code 👉🏼 #!/usr/bin/python
 
 # Second line 👆🏼 should be blank
-#An empty line should be added at the end
+# An empty line should be added at the end
+"""
+Logical Operators (and, or, not, ...)
+    Used to check if two or more conditional statements are true
+"""
+temp = int(input("What is the temperature (*C) outside? "))
+if temp >= 10 and temp <= 30:  # Both conditions must be true
+    print("The temperature is good. \nYou can go outside.")
+elif temp < 10 or temp > 30:  # One of the conditions must be true
+    print("The temperature is bad. \nYou can't go outside.")
+elif not(temp >= 10 and temp <= 30):  # Flips conditions from true to false, false to true
+    print("The temperature is too bad! \nYou can't go outside.")
